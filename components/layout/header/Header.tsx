b8dff60b3078e9c1ea094cbc719a2942332c8a5b
rustoma <rutoma00@gmail.com>
2023-09-07 19:36:34 +0200

    refactor: remove unused Link

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,19 +1,14 @@
 "use client";
 import React from "react";
-import Link from "next/link";
 
 import { Container } from "@/components/container/Container";
 
 import "./header.style.scss";
 
-
 export const Header = () => {
-
   return (
     <header>
-      <Container>
-        Header
-      </Container>
+      <Container>Header</Container>
     </header>
   );
 };
