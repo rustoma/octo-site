480202304df34b0071437f96961f90a184cc95b9
rustoma <rutoma00@gmail.com>
2023-12-10 19:10:31 +0100

    feat: add category page content

--- app/(subpages)/[category]/page.tsx
+++ app/(subpages)/[category]/page.tsx
@@ -1,11 +1,110 @@
 import React from "react";
+import Image from "next/image";
 
+import { Container } from "@/components/container/Container";
+import { Author } from "@/features/author/types";
 import { SimpleBanner } from "@/features/banners/components/simpleBanner/SimpleBanner";
+import { ListViewItem } from "@/features/posts/components/listViewWithSidebar/ListViewItem";
+import { Category, Post } from "@/features/posts/types";
+import { Advertisement } from "@/features/widgets/components/advertisment/Advertisement";
+import { TagsCloud } from "@/features/widgets/components/tagsCloud/TagsCloud";
+
+import "./page.style.scss";
+
+const AUTHORS: Author[] = [
+  {
+    id: 1,
+    name: "John",
+    imageUrl: "https://picsum.photos/100/100",
+  },
+];
+
+const categories: Category[] = [
+  {
+    id: 1,
+    title: "Technology",
+    url: "#",
+  },
+  {
+    id: 2,
+    title: "Mobile",
+    url: "#",
+  },
+  {
+    id: 3,
+    title: "Android Dev",
+    url: "#",
+  },
+  {
+    id: 4,
+    title: "Blockchain",
+    url: "#",
+  },
+];
+
+const gridItItems: Post[] = [
+  {
+    id: 11,
+    title: "Rocket Lab mission fails shortly after launch",
+    url: "#",
+    imageUrl: "https://picsum.photos/650/650",
+    category: categories[0],
+    author: AUTHORS[0],
+  },
+  {
+    id: 12,
+    title: "Rocket Lab mission fails shortly after launch",
+    url: "#",
+    imageUrl: "https://picsum.photos/300/200",
+    category: categories[0],
+    author: AUTHORS[0],
+  },
+  {
+    id: 13,
+    title: "Rocket Lab mission fails shortly after launch",
+    url: "#2",
+    imageUrl: "https://picsum.photos/300/200",
+    category: categories[0],
+    author: AUTHORS[0],
+  },
+  {
+    id: 14,
+    title: "Rocket Lab mission fails shortly after launch",
+    url: "#3",
+    imageUrl: "https://picsum.photos/300/200",
+    category: categories[0],
+    author: AUTHORS[0],
+  },
+  {
+    id: 15,
+    title: "Rocket Lab mission fails shortly after launch",
+    url: "#5",
+    imageUrl: "https://picsum.photos/300/200",
+    category: categories[0],
+    author: AUTHORS[0],
+  },
+];
 
 const CategoryPage = () => {
   return (
     <>
-      <SimpleBanner />
+      <SimpleBanner title="Mobile" />
+      <Container>
+        <div className="category-page">
+          <div className="category-page__content">
+            {gridItItems.map((item) => (
+              <ListViewItem key={item.id} item={item} />
+            ))}
+            <div className="category-page__content-advertisement">
+              <Image src="/img/jpg/advertisement-long.jpg" alt="reklama" width={810} height={115} />
+            </div>
+          </div>
+          <div className="category-page__sidebar">
+            <TagsCloud />
+            <Advertisement />
+          </div>
+        </div>
+      </Container>
     </>
   );
 };
--- features/banners/components/simpleBanner/SimpleBanner.tsx
+++ features/banners/components/simpleBanner/SimpleBanner.tsx
@@ -5,12 +5,16 @@ import { Breadcrumbs } from "@/features/breadcrumbs/components/Breadcrumbs";
 
 import "./simpleBanner.style.scss";
 
-export const SimpleBanner = () => {
+interface SimpleBannerProps {
+  title: string;
+}
+
+export const SimpleBanner = ({ title }: SimpleBannerProps) => {
   return (
     <div className="simple-banner">
       <Container>
         <Breadcrumbs />
-        <h1 className="simple-banner__title">Hello</h1>
+        <h1 className="simple-banner__title">{title}</h1>
       </Container>
     </div>
   );
