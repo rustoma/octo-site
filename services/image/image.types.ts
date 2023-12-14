8835957f3d24d51d8f5800a0b266592c9f17e08f
rustoma <rutoma00@gmail.com>
2023-12-14 19:47:59 +0100

    feat: add image and imageCategory types

---
+++ services/image/image.types.ts
@@ -0,0 +1,13 @@
+export interface Image {
+  ID: number;
+  Name: string;
+  Path: string;
+  Size: number;
+  Type: string;
+  Width: number;
+  Height: number;
+  Alt: string;
+  CategoryId: number;
+  CreatedAt: string;
+  UpdatedAt: string;
+}
