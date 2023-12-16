9bccb42977b9b61f5e9a4700d227e6c416cc90ca
rustoma <rutoma00@gmail.com>
2023-12-16 08:54:39 +0100

    feat: add null as possible type returned from article, category service

--- services/article/article.service.ts
+++ services/article/article.service.ts
@@ -2,7 +2,7 @@ import { httpClient } from "@/features/http";
 import { ARTICLE_ROUTES } from "@/services/article/article.routes";
 import { Article, ArticleFilters } from "@/services/article/article.types";
 
-export const getArticles = async (filters?: ArticleFilters): Promise<Article[]> => {
+export const getArticles = async (filters?: ArticleFilters): Promise<Article[] | null> => {
   const res = await httpClient.get(ARTICLE_ROUTES.index(filters));
   if (!res.ok) {
     throw new Error(`${res.status} ${res.statusText}`);
--- services/category/category.service.ts
+++ services/category/category.service.ts
@@ -2,7 +2,7 @@ import { httpClient } from "@/features/http";
 import { CATEGORY_ROUTES } from "@/services/category/category.routes";
 import { Category, CategoryFilters } from "@/services/category/category.types";
 
-export const getCategories = async (filters?: CategoryFilters): Promise<Category[]> => {
+export const getCategories = async (filters?: CategoryFilters): Promise<Category[] | null> => {
   const res = await httpClient.get(CATEGORY_ROUTES.index(filters));
   if (!res.ok) {
     throw new Error(`${res.status} ${res.statusText}`);
