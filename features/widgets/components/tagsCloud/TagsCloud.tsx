ac9b3ebec25a312c2bb64c270f72b6874e87177c
rustoma <rutoma00@gmail.com>
2023-12-16 08:55:29 +0100

    feat: fetch categories for TagsCloud

--- features/widgets/components/tagsCloud/TagsCloud.tsx
+++ features/widgets/components/tagsCloud/TagsCloud.tsx
@@ -1,41 +1,21 @@
 import React from "react";
 import Link from "next/link";
 
-import { Category } from "@/features/posts/types";
+import { getCategories } from "@/services/category/category.service";
 
 import "./tagsCloud.style.scss";
 
-const categories: Category[] = [
-  {
-    id: 1,
-    title: "Technology",
-    url: "#",
-  },
-  {
-    id: 2,
-    title: "Mobile",
-    url: "#",
-  },
-  {
-    id: 3,
-    title: "Android Dev",
-    url: "#",
-  },
-  {
-    id: 4,
-    title: "Blockchain",
-    url: "#",
-  },
-];
+export const TagsCloud = async () => {
+  const categories = await getCategories();
+  if (!categories) return null;
 
-export const TagsCloud = () => {
   return (
     <div className="tags-cloud">
       <h5 className="tags-cloud__title">Kategorie</h5>
       <div className="tags-cloud__items">
         {categories.map((item) => (
-          <Link key={item.id} href={item.url} className="tags-cloud__item">
-            {item.title}
+          <Link key={item.id} href={`/${item.slug}`} className="tags-cloud__item">
+            {item.name}
           </Link>
         ))}
       </div>
