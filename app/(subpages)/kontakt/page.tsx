c734cfaa9b43e9ddef38fcfe71a0138f7412d0e6
rustoma <rutoma00@gmail.com>
2023-12-17 10:08:31 +0100

    feat: get real email address for contact page

--- app/(subpages)/kontakt/page.tsx
+++ app/(subpages)/kontakt/page.tsx
@@ -6,22 +6,34 @@ import { SimpleBanner } from "@/features/banners/components/simpleBanner/SimpleB
 import { Advertisement } from "@/features/widgets/components/advertisment/Advertisement";
 import { TagsCloud } from "@/features/widgets/components/tagsCloud/TagsCloud";
 import { getBasicPageBySlug } from "@/services/basicPage/basicPage.service";
+import { getDomain } from "@/services/domain/domain.service";
+import { getDomainId } from "@/utils";
 
 import "./page.style.scss";
 
 const ContactPage = async () => {
+  const domainId = getDomainId();
   const page = await getBasicPageBySlug("contact");
 
   if (!page) {
     return notFound();
   }
 
+  const domain = await getDomain(domainId);
+
   return (
     <>
       <SimpleBanner title={page.title} />
       <Container>
         <div className="contact-page">
-          <div className="contact-page__content">{parse(page.body ?? "")}</div>
+          <div className="contact-page__content">
+            {parse(page.body ?? "")}
+            {domain && (
+              <div>
+                Email: <a href={`mailto:${domain.email}`}>{domain?.email}</a>
+              </div>
+            )}
+          </div>
           <div className="contact-page__sidebar">
             <TagsCloud />
             <Advertisement />
