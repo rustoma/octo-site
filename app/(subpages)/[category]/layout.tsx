8007128b863ecbfe547253894e8ab73f9b533473
rustoma <rutoma00@gmail.com>
2023-12-17 07:12:50 +0100

    feat: add layout with generateStaticParams for category

---
+++ app/(subpages)/[category]/layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+import { getCategoriesByDomain } from "@/services/category/category.service";
+
+export const generateStaticParams = async () => {
+  if (!process.env.DOMAIN_ID) {
+    return [];
+  }
+
+  const categories = await getCategoriesByDomain(process.env.DOMAIN_ID);
+
+  return categories?.map((category) => ({
+    category: category.slug,
+  }));
+};
+
+const Layout = ({ children }: { children: ReactNode }) => children;
+
+export default Layout;
