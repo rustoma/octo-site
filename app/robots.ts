12788a5ee2a369062e438f5e802ff440e0df9883
rustoma <rutoma00@gmail.com>
2023-12-27 22:38:24 +0100

    fix: add robots no follow

--- app/robots.ts
+++ app/robots.ts
@@ -4,9 +4,9 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      disallow: "/",
     },
-    host: "https://octo.pl",
-    sitemap: "https://octo.pl/sitemap.xml",
+    host: process.env.FRONTEND_HOST,
+    sitemap: `${process.env.FRONTEND_HOST}/sitemap.xml`,
   };
 }
