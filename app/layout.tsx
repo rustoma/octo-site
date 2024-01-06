be961d2c4e1952a6d88590e5ccaf618955a19f80
rustoma <rutoma00@gmail.com>
2024-01-06 08:29:02 +0100

    feat: add new logo and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode, Suspense } from "react";
-import { Red_Hat_Display } from "next/font/google";
+import { Montserrat, Red_Hat_Display } from "next/font/google";
 
 import { WindowDimensionsProvider } from "@/context/WindowDimensionsContext";
 import { GTM } from "@/features/analytics/components/GTM";
@@ -17,6 +17,19 @@ const redHatDisplay = Red_Hat_Display({
   subsets: ["latin", "latin-ext"],
 });
 
+const montserrat = Montserrat({
+  weight: ["400", "500", "700", "900"],
+  display: "swap",
+  variable: "--theme-font-display",
+  fallback: ["sans-serif"],
+  subsets: ["latin", "latin-ext"],
+});
+
+const displayFont = {
+  redHatDisplay,
+  montserrat,
+};
+
 export const metadata = {
   metadataBase: new URL(process.env.FRONTEND_HOST ?? ""),
   title: `${process.env.DOMAIN_NAME} | ${new Date().getFullYear()}`,
@@ -24,14 +37,18 @@ export const metadata = {
     images: `/img/jpg/social-${getDomainId()}.jpg`,
   },
   icons: {
-    icon: `/favicons/favicon-${getDomainId()}.png`
-  }
+    icon: `/favicons/favicon-${getDomainId()}.png`,
+  },
 };
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <WindowDimensionsProvider>
-      <html lang="pl" className={`${redHatDisplay.variable} light`}>
+      <html
+        lang="pl"
+        className={`${
+          displayFont[(process.env.DISPLAY_FONT ?? "redHatDisplay") as keyof typeof displayFont].variable
+        } light`}>
         <body>
           {children}
           <CookieBanner />
