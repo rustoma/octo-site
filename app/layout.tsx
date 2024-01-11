d73edb5aa2d4e311dba614e6f02f2fd46dd757b8
rustoma <rutoma00@gmail.com>
2024-01-11 09:29:44 +0100

    feat: add theme dark construction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode, Suspense } from "react";
-import { Montserrat, Red_Hat_Display } from "next/font/google";
+import { Montserrat, Red_Hat_Display, Roboto } from "next/font/google";
 
 import { WindowDimensionsProvider } from "@/context/WindowDimensionsContext";
 import GoogleAnalytics from "@/features/analytics/components/GoogleAnalytics";
@@ -25,9 +25,18 @@ const montserrat = Montserrat({
   subsets: ["latin", "latin-ext"],
 });
 
+const roboto = Roboto({
+  weight: ["400", "500", "700", "900"],
+  display: "swap",
+  variable: "--theme-font-display",
+  fallback: ["sans-serif"],
+  subsets: ["latin", "latin-ext"],
+});
+
 const displayFont = {
   redHatDisplay,
   montserrat,
+  roboto,
 };
 
 export const metadata = {
