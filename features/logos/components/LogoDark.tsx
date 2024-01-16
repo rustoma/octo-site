527fa958a2ddee6ea8003b1279770cb2cc13050b
rustoma <rutoma00@gmail.com>
2024-01-16 22:03:10 +0100

    feat: add FinanceObr logo

--- features/logos/components/LogoDark.tsx
+++ features/logos/components/LogoDark.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 
+import { FinanceObrLight } from "@/features/logos/components/financeObr/FinanceObrLight";
 import { FloorServiceDark } from "@/features/logos/components/floorService/FloorServiceDark";
 import { RoofServiceLight } from "@/features/logos/components/roofService/RoofServiceLight";
 import { getDomainId } from "@/utils";
@@ -7,6 +8,7 @@ import { getDomainId } from "@/utils";
 const LOGOS: Record<string, ReactElement> = {
   1: <FloorServiceDark />,
   2: <RoofServiceLight />,
+  3: <FinanceObrLight />,
 };
 
 export const LogoDark = () => {
--- features/logos/components/LogoLight.tsx
+++ features/logos/components/LogoLight.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 
+import { FinanceObrLight } from "@/features/logos/components/financeObr/FinanceObrLight";
 import { FloorServiceLight } from "@/features/logos/components/floorService/FloorServiceLight";
 import { RoofServiceLight } from "@/features/logos/components/roofService/RoofServiceLight";
 import { getDomainId } from "@/utils";
@@ -7,6 +8,7 @@ import { getDomainId } from "@/utils";
 const LOGOS: Record<string, ReactElement> = {
   1: <FloorServiceLight />,
   2: <RoofServiceLight />,
+  3: <FinanceObrLight />,
 };
 
 export const LogoLight = () => {
