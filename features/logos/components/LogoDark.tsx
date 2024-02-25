18fd4f847663ef17cb04f411d5409f1203986684
rustoma <rutoma00@gmail.com>
2024-02-25 21:49:13 +0100

    feat: add logos and favicons

--- features/logos/components/LogoDark.tsx
+++ features/logos/components/LogoDark.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from "react";
 
 import { BepemamebleDark } from "@/features/logos/components/bepemameble/BepemamebleDark";
+import { BioSerwisDark } from "@/features/logos/components/bioserwis/BioSerwisDark";
 import { CawioDark } from "@/features/logos/components/cawio/CawioDark";
 import { FinanceObrLight } from "@/features/logos/components/financeObr/FinanceObrLight";
 import { FloorServiceDark } from "@/features/logos/components/floorService/FloorServiceDark";
+import { KrismetDark } from "@/features/logos/components/krismet/KrismetDark";
 import { MaximDark } from "@/features/logos/components/maxim/MaximDark";
 import { RoofServiceLight } from "@/features/logos/components/roofService/RoofServiceLight";
 import { getDomainId } from "@/utils";
@@ -14,7 +16,9 @@ const LOGOS: Record<string, ReactElement> = {
   3: <FinanceObrLight />,
   4: <MaximDark />,
   5: <CawioDark />,
+  6: <BioSerwisDark />,
   7: <BepemamebleDark />,
+  8: <KrismetDark />,
 };
 
 export const LogoDark = () => {
--- features/logos/components/LogoLight.tsx
+++ features/logos/components/LogoLight.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from "react";
 
 import { BepemamebleLight } from "@/features/logos/components/bepemameble/BepemamebleLight";
+import { BioSerwisLight } from "@/features/logos/components/bioserwis/BioSerwisLight";
 import { CawioLight } from "@/features/logos/components/cawio/CawioLight";
 import { FinanceObrLight } from "@/features/logos/components/financeObr/FinanceObrLight";
 import { FloorServiceLight } from "@/features/logos/components/floorService/FloorServiceLight";
+import { KrismetLight } from "@/features/logos/components/krismet/KrismetLight";
 import { MaximLight } from "@/features/logos/components/maxim/MaximLight";
 import { RoofServiceLight } from "@/features/logos/components/roofService/RoofServiceLight";
 import { getDomainId } from "@/utils";
@@ -14,7 +16,9 @@ const LOGOS: Record<string, ReactElement> = {
   3: <FinanceObrLight />,
   4: <MaximLight />,
   5: <CawioLight />,
+  6: <BioSerwisLight />,
   7: <BepemamebleLight />,
+  8: <KrismetLight />,
 };
 
 export const LogoLight = () => {
